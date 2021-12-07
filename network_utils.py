f2eab4be3190573db93ed9613b655e1d3c948617
Shuang Ao <82127788+Shuang-AO@users.noreply.github.com>
2021-12-08 01:32:14 +0800

    Add files via upload

--- network_utils.py
+++ network_utils.py
@@ -28,19 +28,3 @@ def optimize_by_loss(loss, parameters_to_optimize, learning_rate, gradient_limit
         optimize_op = optimizer.apply_gradients(zip(gradients, variables))
         return initial_gradients_norm, clipped_gradients_norm, optimize_op
 
-trainer_rl = train_eval(tf_agent, tf_env, eval_tf_env, interations,
-                        steps, batch_size, episodes, interval, log_interval, seed)
-for current_level in range(config['model']['levels'],config['model']['starting_level']-1,-1):
-
-    best_cost, best_cost_global_step = None, None
-    no_test_improvement, consective_learn_rate_decrease = 0, 0
-
-    for cycle in range(config['general']['training_cycles_per_level']):
-        print_and_log('current cycle {}, level {}'.format(cycle, current_level))
-
-        global_step, train_loss = trainer_rl.train_evla(current_level, global_step)
-        if new_global_step == global_step:
-            print_and_log('tag: no data found - training cycle {} - global step {}'.format(cycle, global_step))
-            continue
-        else:
-            global_step = new_global_step
